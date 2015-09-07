b56545bb34fee29699b311540a758f38f55fe98f
Sam Phillips <samuel.rodney.phillips@gmail.com>
2015-09-07 12:36:12 +0200

    created landing_page view, orders controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   resources :products
+  root 'static_pages#landing_page'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
 
+  resources :orders, only: [:index, :show, :new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  # root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
