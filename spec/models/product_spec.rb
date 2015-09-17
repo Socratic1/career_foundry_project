3b483b6ef6c3a9a8d7b43ae07867e247d4d94ba0
Sam <samuel.rodney.phillips@gmail.com>
2015-09-17 13:29:00 +0200

    unit tests trial 5000

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -34,36 +34,31 @@ describe Product do
         before do
             @user = User.new(email: "example@example.com", 
                                 password: "examplepass",
-                                password_confirmation: "examplepass")            
+                                password_confirmation: "examplepass")       
             @product = Product.new(name: "Example Bike", 
                                       description: "For testing",
                                       image_url: "example.jpg", 
                                       colour: "red")
             @product.save
             comment1 = @product.comments.create(user: @user.id,
-                                                product: @product,
                                                 rating: 5, 
                                                 body: "Test comment")
             comment2 = @product.comments.create(user: @user.id, 
-                                                product: @product.id,
                                                 rating: 4, 
                                                 body: "Test comment")
             comment3 = @product.comments.create(user: @user.id, 
-                                                product: @product.id,
                                                 rating: 2, 
                                                 body: "Test comment")
             comment4 = @product.comments.create(user: @user.id, 
-                                                product: @product.id,
                                                 rating: 3, 
                                                 body: "Test comment")
             comment5 = @product.comments.create(user: @user.id, 
-                                                product: @product.id,
                                                 rating: 0, 
                                                 body: "Test comment")
         end
 
         it "should return the average rating for @product" do
-            expect(@product.comments.average(:rating)).to eq((5 + 4 + 2 + 3) / 5)
+            expect(@product.comments.average(:rating).to_f).to eq((5.0 + 4 + 2 + 3) / 5)
         end
     end
 end
