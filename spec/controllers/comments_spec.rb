0e2f64e0cdc72f9b73ff3cdd56f271a5e3c6557e
Sam <samuel.rodney.phillips@gmail.com>
2015-09-26 14:50:56 +0200

    stuff with comments_spec.rb

--- spec/controllers/comments_spec.rb
+++ spec/controllers/comments_spec.rb
@@ -1,81 +1,80 @@
 require 'rails_helper'
 
 RSpec.configure do |config|
-	config.include Devise::TestHelpers, type: :controller
-	describe CommentsController, :type => :controller do
-		render_views
+    config.include Devise::TestHelpers, type: :controller
+    describe CommentsController, :type => :controller do
+        render_views
 
-		before do
-			@user = create(:user)
-			@product = create(:product)
-			@comment = @product.comments.create(
-				user: @user,
-				product: @product,
+        before do
+            @user = create(:user)
+            @product = create(:product)
+            @comment = @product.comments.create(
+                user: @user,
                 rating: 5, 
                 body: "Test comment"
             )
 
         end
 
-		context "show comments" do
-			it "responds successfully" do
-			end
+        context "show comments" do
+            it "responds successfully" do
+            end
 
-			it "renders the products/@product.id template" do
-			end
-		end
+            it "renders the products/@product.id template" do
+            end
+        end
 
-		context "create comment" do
+        context "create comment" do
 
-			before do
-           		sign_in :user, @user
-			end
+            before do
+                sign_in :user, @user
+            end
 
-			it "successfully creates new comment" do
-				expect{ 
-					post :create,
-					product_id: @product.id, 
-					comment: {
-						user: @user, 
-						rating: 5, 
-						body: "Test Comment"
-					}
-				}.to change{ @product.comments.count }.by(1)
-				assert_redirected_to product_path(assigns(:product))
-			end
-		end
+            it "successfully creates new comment" do
+                expect{ 
+                    post :create,
+                    product_id: @product.id, 
+                    comment: {
+                        user: @user, 
+                        rating: 5, 
+                        body: "Test Comment"
+                    }
+                }.to change{ @product.comments.count }.by(1)
+                assert_redirected_to product_path(assigns(:product))
+            end
+        end
 
-		context "destroy comment" do
-			context "logged in as admin" do
+        context "destroy comment" do
+            context "logged in as admin" do
 
-				before do
-					@user = create(:admin, email: "example2@example.com")
-            		sign_in :user, @user
-				end
+                before do
+                    @user = create(:admin, email: "example2@example.com")
+                    sign_in :user, @user
+                end
 
-				it "deletes @comment" do
-					expect{ 
-						delete :destroy, 
-						product_id: @product.id, 
-						id: @comment.id
-					}.to change{ Comment.count }.by(-1)
-					assert_redirected_to product_path(assigns(:product))
-				end
-			end
+                it "deletes @comment" do
+                    expect{ 
+                        delete :destroy, 
+                        product_id: @product.id, 
+                        id: @product.comments.first.id
+                    }.to change{ Comment.count }.by(-1)
+                    assert_redirected_to product_path(assigns(:product))
+                end
+            end
 
-			context "not logged in as admin" do
-				before do
-            		sign_in :user, @user
-				end
+            context "not logged in as admin" do
+                before do
+                    sign_in :user, @user
+                end
 
-				it "does not delete @comment" do
-					expect{ delete :destroy, id: @comment }.to change{ Comment.count }.by(0)
-					assert_redirected_to product_path(assigns(:product))
-				end
+                it "does not delete @comment" do
+                    expect{ delete :destroy, id: @comment }.to change{ Comment.count }.by(0)
+                    assert_redirected_to product_path(assigns(:product))
+                end
 
-			end
-		end
-	end
+            end
+        end
+    end
 
 end
 
