0a90e226c9040d970c695568d1aa2fbe3701b3d6
Sam <samuel.rodney.phillips@gmail.com>
2015-10-08 14:03:15 +0200

    creating orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,7 +17,7 @@ class OrdersController < ApplicationController
 		@order.product = Product.find(params[:product_id])
 		@order.user = current_user
 
-    	respond_to do |format|
+		respond_to do |format|
       		if @order.save
         		format.html { redirect_to @order.product, notice: 'Your order was successful.' }
         		format.json { render :show, status: :created, location: @order.product }
@@ -26,6 +26,32 @@ class OrdersController < ApplicationController
         		format.json { render json: @order.errors, status: :unprocessable_entity }
       		end
     	end
+
+		customer = Stripe::Customer.retrieve(params[:customer_id])
+
+		begin
+			charge = Stripe::Charge.create(
+				:amount => @order.product.amount, #amount in cents, again
+				:currency => "eur",
+				:source => token, 
+				:description => params[:stripeEmail],
+				:customer => customer.id,
+				:metadata => {"order_id" => @order.id}
+			)
+
+			@email = @user[:email]
+			@message = "Thank for you purchasing the #{ @product[:name] }."
+			ActionMailer::Base.mail(:from => 'staff@leuvenbikes.com',
+				:to => @email,
+				:subject => 'Purchase Confirmation',
+				:body => @message).deliver
+
+		rescue Stripe::CardError => e
+			# The card has been declined
+			body = e.json_body
+			err = body[:error]
+			flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+		end
 	end
 
 end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,29 +5,15 @@ class PaymentsController < ApplicationController
 		@user = current_user
 
 		token = params[:stripeToken]
-		# Create the charge on Stripe's servers - this will charge the user's card
-		begin
-			charge = Stripe::Charge.create(
-				:amount => @product.amount, #amount in cents, again
-				:currency => "eur",
-				:source => token, 
-				:description => params[:stripeEmail]
-				)
+		customer = Stripe::Customer.create(
+			:source => token,
+			:email => @user.email
+		)
 
-			@email = @user[:email]
-			@message = "Thank for you purchasing the #{ @product[:name] }."
-			ActionMailer::Base.mail(:from => 'staff@leuvenbikes.com',
-				:to => @email,
-				:subject => 'Purchase Confirmation',
-				:body => @message).deliver
-
-		rescue Stripe::CardError => e
-			# The card has been declined
-			body = e.json_body
-			err = body[:error]
-			flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-		end
-		redirect_to new_order_url(:user_id => @user.id, :product_id => @product.id)
+		redirect_to new_order_url(:user_id => @user.id, 
+								  :product_id => @product.id,
+								  :customer_id => customer.id
+		)
 	end
 
 
