c42b511424ed9401c197cfc5087466b93fd00aaf
Sam <samuel.rodney.phillips@gmail.com>
2015-10-10 13:07:49 +0200

    fixing orders 2

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
 	Rails.configuration.stripe = {
-	    :publishable_key => ENV['pk_live_I4QWiic3kDy7RaLxMy6VaDIp'],
+	    :publishable_key => ENV['pk_test_oaLwIT5RZOrZJkMNF8baC9R0'],
 	    :secret_key => ENV['sk_live_bBiphgwI9ClXruEWqZDFsPgH']
 	}
 else
     Rails.configuration.stripe = {
-    	:publishable_key => 'pk_test_SlTylXi4BMo0QI1vcjz8ucVa',
+    	:publishable_key => 'pk_test_oaLwIT5RZOrZJkMNF8baC9R0',
     	:secret_key => 'sk_test_cAIasZ0q7hi6a22jrbdXzx6W'
     }
 end
