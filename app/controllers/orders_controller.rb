e3a6adec0f0e099bc58fceeb33b9075c129e0f34
Sam <samuel.rodney.phillips@gmail.com>
2015-10-10 13:38:00 +0200

    fixing orders 8

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,51 +1,52 @@
 class OrdersController < ApplicationController
 
-	def index
-	end
+    def index
+    end
 
-	def show
-	end
+    def show
+    end
 
-	def new
-		@order = Order.new
-		@order.user = User.find(params[:user_id])
-		@order.product = Product.find(params[:product_id])
-		@customer = Stripe::Customer.retrieve(params[:customer_id])
-	end
+    def new
+        @order = Order.new
+        @order.user = User.find(params[:user_id])
+        @order.product = Product.find(params[:product_id])
+        @customer = Stripe::Customer.retrieve(params[:customer_id])
+    end
 
-	def create
-		@order = Order.new
-		@order.product = Product.find(params[:product_id])
-		@order.user = current_user
+    def create
+        @order = Order.new
+        @order.product = Product.find(params[:product_id])
+        @order.user = current_user
 
-		@customer = Stripe::Customer.retrieve(params[:customer_id])
+        @customer = Stripe::Customer.retrieve(params[:customer_id])
 
-		begin
-			charge = Stripe::Charge.create(
-				:amount => @order.product.amount, #amount in cents, again
-				:currency => "eur",
-				:description => params[:stripeEmail],
-				:customer => @customer.id,
-				:metadata => {"order_id" => @order.id}
-			)
+        begin
+            charge = Stripe::Charge.create(
+                :amount => @order.product.amount, #amount in cents, again
+                :currency => "eur",
+                :description => params[:stripeEmail],
+                :customer => @customer.id,
+                :metadata => {"order_id" => @order.id}
+            )
 
 
-		rescue Stripe::CardError => e
-			# The card has been declined
-			body = e.json_body
-			err = body[:error]
-			flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-		end
-	end
+        rescue Stripe::CardError => e
+            # The card has been declined
+            body = e.json_body
+            err = body[:error]
+            flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+        end
+    
 
-	respond_to do |format|
-      	if @order.save
-        	format.html { redirect_to @order.product, notice: 'Your order was successful.' }
-        	format.json { render :show, status: :created, location: @order.product }
-      	else
-        	format.html { render :new }
-        	format.json { render json: @order.errors, status: :unprocessable_entity }
-      	end
+        respond_to do |format|
+            if @order.save
+                format.html { redirect_to @order.product, notice: 'Your order was successful.' }
+                format.json { render :show, status: :created, location: @order.product }
+            else
+                format.html { render :new }
+                format.json { render json: @order.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
 end
